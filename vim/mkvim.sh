1ebb6ace0149a5577c243bd94c0af1f1cb98a6e0
kevin <kevin.wang2004@hotmail.com>
2012-12-07 08:57:25 +0800

    添加编译者这个选项

--- vim/mkvim.sh
+++ vim/mkvim.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-./configure --enable-multibyte --with-ruby-command=ruby --enable-rubyinterp=yes --with-features=huge --enable-fontset --enable-cscope --with-x --enable-xim --enable-pythoninterp
+./configure --with-compiledby=kevin.wang --enable-multibyte --with-ruby-command=ruby --enable-rubyinterp=yes --with-features=huge --enable-fontset --enable-cscope --with-x --enable-xim --enable-pythoninterp
