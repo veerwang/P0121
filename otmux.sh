de7ebc20364f958af68e55053aada5233141403f
kevin <kevin.wang2004@hotmail.com>
2013-10-09 15:00:01 +0800

    添加otmux.sh脚本

---
+++ otmux.sh
@@ -0,0 +1,44 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: otmux.sh
+# 
+#         USAGE: ./otmux.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2013年10月09日 13:35
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+cmd=$(which tmux) 			    # tmux path 
+session=basic
+
+if [ -z $cmd ]; then
+	echo "You need to install tmux."
+	exit 1
+fi
+
+$cmd -2 has -t $session
+
+if [ $? != 0 ]; then
+	$cmd new -d -n vim -s $session "vim"
+	$cmd neww -n telnet -t $session "bash"
+	$cmd neww -n sdcv -t $session "sdcv"
+	$cmd splitw -v -p 70 "python"
+	$cmd splitw -h -p 50 "top"
+	$cmd neww -n mutt -t $session "mutt"
+	$cmd selectw -t $session:1
+fi
+
+$cmd -2 att -t $session
+
+exit 0
