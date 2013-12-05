c56ef852b5d2a087bdc1615f0e69f9a5a43570fe
kevin <kevin.wang2004@hotmail.com>
2013-12-05 09:32:34 +0800

    将top系统监视工具变换成为htop

--- otmux.sh
+++ otmux.sh
@@ -31,12 +31,12 @@ $cmd -2 has -t $session
 
 if [ $? != 0 ]; then
 	$cmd new -d -n calcurse -s $session "calcurse"
-	$cmd neww -n telnet -t $session "zsh"
+	$cmd neww -n vim -t $session "zsh"
+	$cmd neww -n debug -t $session "zsh"
 	$cmd neww -n sdcv -t $session "sdcv"
 	$cmd splitw -v -p 70 "python"
-	$cmd splitw -h -p 50 "top"
+	$cmd splitw -h -p 50 "htop"
 	$cmd neww -n mutt -t $session "mutt"
-	$cmd neww -n cmus -t $session "cmus"
 	$cmd neww -n vifm -t $session "vifm"
 	$cmd selectw -t $session:1
 fi
