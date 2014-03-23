714ca05b9c617a107d626f447cf6bbb491116f09
kevin <kevin.wang2004@hotmail.com>
2014-03-23 22:14:29 +0800

    修正otmux.sh脚本

--- otmux.sh
+++ otmux.sh
@@ -31,14 +31,19 @@ $cmd -2 has -t $session
 
 if [ $? != 0 ]; then
 	$cmd new -d -n calcurse -s $session "calcurse"
-	$cmd neww -n vim -t $session "zsh"
+	$cmd neww -n Ruby -t $session:2 "zsh"
+	$cmd neww -n vim2 -t $session "zsh"
 	$cmd neww -n debug -t $session "zsh"
 	$cmd neww -n sdcv -t $session "sdcv"
 	$cmd splitw -v -p 70 "python"
 	$cmd splitw -h -p 50 "htop"
 	$cmd neww -n mutt -t $session "mutt"
-	$cmd neww -n vifm -t $session "vifm"
-	$cmd selectw -t $session:1
+	$cmd neww -n cmus -t $session "cmus"
+	$cmd neww -n mc -t $session "mc"
+	$cmd selectw -t $session:2
+
+	$cmd send-keys -t $session:2 "cd /home/kevin/armworkcopy/Ruby" C-m
+	$cmd send-keys -t $session:2 "vim README" C-m
 fi
 
 $cmd -2 att -t $session
