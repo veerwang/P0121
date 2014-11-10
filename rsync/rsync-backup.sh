c9fc59c392dd0386a4a2e3556fa35a2e1b6070a8
kevin <kevin.wang2004@hotmail.com>
2014-11-10 17:07:11 +0800

    添加rsync程序的差异备份脚本

---
+++ rsync/rsync-backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: rsync-backup.sh
+# 
+#         USAGE: ./rsync-backup.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2014年11月10日 14:11
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+rsync -avz --delete --progress resource save
