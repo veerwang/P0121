304863c783d5aabe099ac0f8a8a7d3c999abb039
kevin <kevin.wang2004@hotmail.com>
2015-07-31 13:22:05 +0800

    修正mkvim.sh的文件,编译vim的选项

--- vim/mkvim.sh
+++ vim/mkvim.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-./configure --with-compiledby=kevin.wang --enable-multibyte --with-ruby-command=ruby --enable-rubyinterp=yes --with-features=huge --enable-fontset --enable-cscope --with-x --enable-xim --enable-pythoninterp --disable-gui
+./configure --with-compiledby=kevin.wang --enable-multibyte --with-ruby-command=ruby --enable-rubyinterp=yes --with-features=huge --enable-fontset --enable-cscope --enable-gnome-check=yes --enable-gui=no --enable-xim --enable-pythoninterp --enable-python3interp
