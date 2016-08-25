4a1914acb04fcd563006ef253fd5e01c8b494180
kevin.wang <wwei@landicorp.com>
2016-08-25 13:30:09 +0800

    添加价格文件

--- ledger/run.sh
+++ ledger/run.sh
@@ -17,7 +17,8 @@
 #      REVISION:  ---
 #===============================================================================
 
+#ledger  -f kevin-ledger.ldg bal 股票 --price-db price.db -V
 set -o nounset                              # Treat unset variables as an error
 ledger -f kevin-ledger.ldg -S T bal 资产 负债 > 权益
 ledger -M -f kevin-ledger.ldg reg 支出 > 月度支出
-ledger -f kevin-ledger.ldg bal 资产:股票 > 股票
+ledger -f kevin-ledger.ldg bal --price-db price.db -V 资产:股票 > 股票
