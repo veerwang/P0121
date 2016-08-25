707064ff9cc88e03dd4d1ad2c4bd8c72998f45b7
kevin <kevin.wang2004@hotmail.com>
2016-08-25 19:45:53 +0800

    添加查询脚本

---
+++ ledger/report-ledger.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: run.sh
+# 
+#         USAGE: ./run.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2016年08月25日 11:30
+#      REVISION:  ---
+#===============================================================================
+
+#ledger  -f kevin-ledger.ldg bal 股票 --price-db price.db -V
+set -o nounset                              # Treat unset variables as an error
+ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg -S T --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V bal 资产 负债 > 权益
+ledger -M -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出 > 月度支出
+ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg bal --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V 资产:股票 > 股票
