25f614c20b93716a72a5483336ec20c3c30e09c0
kevin <kevin.wang2004@hotmail.com>
2016-09-03 22:41:21 +0800

    修正ledger报告脚本

--- ledger/report-ledger.sh
+++ ledger/report-ledger.sh
@@ -20,6 +20,6 @@
 #ledger  -f kevin-ledger.ldg bal 股票 --price-db price.db -V
 set -o nounset                              # Treat unset variables as an error
 ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg -S T --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V bal 资产 负债 > 权益
-ledger -b "this month" -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出 > 本月支出明细
+ledger -b "this month" -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出 -S 'amount' > 本月支出明细
 ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg bal --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V 资产:股票 > 股票
-ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg -b "this month" --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V reg 收入 支出 > 本月收支表 
+ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg -b "this month" --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V -S 'amount' reg 收入 支出 > 本月收支表 
