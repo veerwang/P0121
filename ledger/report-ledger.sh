36f319247ae04b91e3a666d6e32c2e94b42f0634
kevin <kevin.wang2004@hotmail.com>
2016-11-01 22:32:03 +0800

    修改账本

--- ledger/report-ledger.sh
+++ ledger/report-ledger.sh
@@ -21,8 +21,11 @@
 set -o nounset                              # Treat unset variables as an error
 ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg -S T --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V bal 资产 负债 > 权益
 ledger -b "this month" -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出 -S 'amount' > 本月支出明细
-ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出:交通费用:汽车加油 -S 'amount' > 汽油
+ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出:交通费用:汽车加油 -M > 汽油
 ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出:Jack研发费用 -S 'amount' > Jack 
 ledger -b "today" -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg reg 支出 -S 'amount' > 今日支出明细
 ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg bal --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V 资产:股票 > 股票
 ledger -f ~/DataLibrary/kevin.wang-account/ledger/kevin-ledger.ldg -b "this month" --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V -S 'amount' reg 收入 支出 > 本月收支表 
+
+
+ledger -f ~/DataLibrary/kevin.wang-account/ledger/wangwang.ldg -S T --price-db ~/DataLibrary/kevin.wang-account/ledger/price.db -V bal 资产 负债 > wangwang权益
