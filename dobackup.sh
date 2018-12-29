afef8e81446767652643e81354a8c094bcdbcd7a
kevin.wang <wwei@landicorp.com>
2018-12-29 17:35:58 +0800

    添加备份脚本

---
+++ dobackup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: dotransfer.sh
+# 
+#         USAGE: ./dotransfer.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2018年12月29日 16:34
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+read -t 8 -p "请输入远方IP地址：$IP" IP 
+IP=${IP:-"172.27.16.28"}
+
+echo "IP=$IP"
+
+if [ $# -ne 2 ]; then
+	echo " Usage:    $0 type director"
+	echo " type:     tr or re. which directory need to be transfered"
+	echo " director: the director need to be transfered"
+	exit
+fi
+
+if [ $1 = "tr" ]; then
+	echo "start transfer data ..."
+	rsync --archive --verbose --delete $2 wwei@$IP:/home/wwei/
+elif [ $1 = "re" ]; then
+	echo "start restore data ..."
+	rsync --archive --verbose --delete wwei@$IP:/home/wwei/$2 ./ 
+else
+	echo " type:     tr or re"
+fi
