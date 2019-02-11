6a2d10190c83484feb4cdb3e55196a2ec8b18797
kevin <kevin.wang2004@hotmail.com>
2019-02-11 21:40:21 +0800

    修正vim编译指令,使之支持python3

--- vim/mkvim.sh
+++ vim/mkvim.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-./configure --with-compiledby=kevin.wang --enable-multibyte --with-ruby-command=ruby --enable-rubyinterp=yes --with-features=huge --enable-fontset --enable-cscope --enable-gnome-check=yes --enable-gui=no --enable-xim --enable-pythoninterp --enable-python3interp
+./configure --with-compiledby=kevin.wang --enable-multibyte --with-ruby-command=ruby --enable-rubyinterp=yes --with-features=huge --enable-fontset --enable-cscope --enable-gnome-check=yes --enable-gui=no --enable-xim --enable-pythoninterp=yes --enable-python3interp=yes --with-python3-command=python --with-python3-config-dir=/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu
