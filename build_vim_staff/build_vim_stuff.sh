57117caa8f3d97fffd0bab92b17ea9b9873fd09a
kevin <kevin.wang2004@hotmail.com>
2019-02-27 21:32:42 +0800

    重新调整菜单

--- build_vim_staff/build_vim_stuff.sh
+++ build_vim_staff/build_vim_stuff.sh
@@ -4,3 +4,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "2.Start <--- Copy vimrc file into ~/.vim/"
     cp -f ./vimrc ~/.vimrc
+echo "3.Start <--- make env"
+    mkdir -p ~/.vim/undodir
+    cp ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
