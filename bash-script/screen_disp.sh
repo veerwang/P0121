47067c38a72711e3c5f6c9bf1588c333ced7c519
kevin.wang <wwei@landicorp.com>
2020-12-24 15:20:09 +0800

    添加界面显示脚本

---
+++ bash-script/screen_disp.sh
@@ -0,0 +1,38 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: screen_disp.sh
+# 
+#         USAGE: ./screen_disp.sh 
+# 
+#   DESCRIPTION: 保存屏幕显示功能的函数 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2020年12月24日 15:09
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+clean_screen() {
+	echo -e "\033[2J"
+}
+
+#
+# 显示屏幕位置的函数
+# $1: x
+# $2: y
+# $3: string
+# $4: color
+print() {
+	echo -ne "$4'\033[$2;$1H'$3'\E[0m'"
+}
+
+clean_screen
+
+print 10 2 测试字符串 '\E[1;31m'
